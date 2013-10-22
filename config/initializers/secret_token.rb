eb048260030c0c3fd7753fb093b4fcda94846527
Weerapat Pittayakanchit <wpittayakanchit@hmc.edu>
2013-10-21 20:39:13 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BetaMentorsApp::Application.config.secret_key_base = '1d415cd415a1a1f0c0eeb73145a40318e96267b654447e8c17c45a12b16f26d95949dc3361ae5e662885da0463b2b634d0a4368b58fc7af9cb8036f5a45f2528'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BetaMentorsApp::Application.config.session_store :cookie_store, key: '_beta_mentors_app_session'
