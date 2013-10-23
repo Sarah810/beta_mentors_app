1026e4bbdb2e0aa59459ccdb03326aff51335537
Weerapat Pittayakanchit <wpittayakanchit@hmc.edu>
2013-10-22 22:02:54 -0700

    Use Select for choosing language instead of radio buttons

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,16 @@ class User < ActiveRecord::Base
 	before_save { self.email = email.downcase }
 	before_create :create_remember_token
 
-	validates :name, presence: true, length: { maximum: 50 }
+	PREFER_LANGUAGE_TYPES = ["Mandarin","English","English and Mandarin"]
+
+	validates :name, presence: true, 
+	                 length: { maximum: 50 }
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 	validates :email, presence: true,
 	                  format: { with: VALID_EMAIL_REGEX },
 	                  uniqueness: { case_sensitive: false }
- 	
+ 	validates :language, :inclusion => PREFER_LANGUAGE_TYPES
+
 	has_secure_password
 	validates :password, length: { minimum: 6 }
 
