84bf107ee0e4afeeeaacff006ee4b25c3764ca60
Weerapat Pittayakanchit <wpittayakanchit@hmc.edu>
2013-10-26 23:57:25 -0700

    add new, edit, index pages for courses

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 BetaMentorsApp::Application.routes.draw do
   
+  resources :courses
+  
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
+
   match '/signup',  to: 'users#new',            via: 'get'
   match '/signin',  to: 'sessions#new',         via: 'get'
   match '/signout', to: 'sessions#destroy',     via: 'delete'
